526b5b2dfefa70848602700f3c486ba8b04cc478
NicotinaaTv <nnicotinaaa@gmail.com>
2026-05-21 22:02:25 +0200

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,5 @@
 window.NICOTINAATV_SUPABASE = {
   url: "https://kdeczcfwszjnqeypluox.supabase.co",
-  publishableKey: "sb_publishable_3MKCaociagPNfDUrT9866g_P6LraZd",
+  publishableKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtkZWN6Y2Z3c3pqbnFleXBsdW94Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkzNzYzNjUsImV4cCI6MjA5NDk1MjM2NX0.YMwzQ_XlB4ohKxrxq-nKt2vLvjynglfFnqELmr6GAek",
   ceoApiUrl: "",
 };
